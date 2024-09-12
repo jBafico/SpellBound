dff9751346e1b33254169c9e789bf532709ae15c
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-09-12 20:02:51 -0300

    chore: added textures and enemy

---
+++ Assets/Scripts/Entities/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    #region CONSTANT_PROPERTIES
+    
+    private string WALK_MOVEMENT_STRATEGY = "CharacterWalk";
+    #endregion
+    
+    #region PROPERTIES
+
+    private CharacterWalk _characterWalk;
+    private IMoveable _movementLogic;
+    
+    #endregion
+    
+    void Start()
+    {
+        AddDynamicComponent(WALK_MOVEMENT_STRATEGY);
+        
+        _characterWalk = GetComponent<CharacterWalk>();
+        _movementLogic = _characterWalk;
+    }
+
+    #region PRIVATE_METHODS
+
+    private void AddDynamicComponent(string name)
+    {
+        Type newComponent = Type.GetType($"{name}");
+
+        if (newComponent != null)
+        {
+            Debug.LogWarning($"Component '{name}' added!");
+            gameObject.AddComponent(newComponent);
+        }
+        else
+        {
+            Debug.LogWarning($"Component '{name}' not found!");
+        }
+    }
+
+    #endregion
+}
