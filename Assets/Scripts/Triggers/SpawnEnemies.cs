403df07d9f3641a5d4cc985a9d6948bf6a7427d7
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-09-25 19:41:24 -0300

    feat: enemy trigger spawner

---
+++ Assets/Scripts/Triggers/SpawnEnemies.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour
+{
+    #region VARIABLES
+
+    [SerializeField] private List<GameObject> _spawnPoints;
+    [SerializeField] private GameObject _enemyPrefab;
+
+    public GameObject EnemyPrefab => _enemyPrefab;
+
+    #endregion
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        foreach (var spawnPoint in _spawnPoints)
+        {
+            Instantiate(EnemyPrefab, spawnPoint.GetComponent<Transform>().position, Quaternion.identity);
+        }
+        Destroy(this);
+    }
+}
