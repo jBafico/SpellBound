3ff812d960a950cea592140112aa5f84de8cba23
Gonzalo Martone <70904263+ImNotGone@users.noreply.github.com>
2024-10-01 16:42:37 -0300

    feat: implement basic menu

--- Assets/Managers/MenuManager.cs
+++ Assets/Managers/MenuManager.cs
@@ -7,13 +7,16 @@ public class MenuManager : MonoBehaviour
 {
         //Le pasamos el loading porque el loading va a cargar el level
         private const string LEVEL_1_SCENE = "Loading";
+        private const string CREDITS_SCENE = "Credits";
 
         [SerializeField] private Button _actionPlay;
+        [SerializeField] private Button _actionCredits;
         [SerializeField] private Button _actionQuit;
 
         private void Start()
         {
                 _actionPlay.onClick.AddListener(()=> LoadSceneByName(LEVEL_1_SCENE));
+                _actionCredits.onClick.AddListener(()=> LoadSceneByName(CREDITS_SCENE));
                 _actionQuit.onClick.AddListener(QuitGame);
         }
 
