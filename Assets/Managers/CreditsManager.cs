5b564c5825b2f0c65ef7e64ba6caf202b868b21d
Gonzalo Martone <70904263+ImNotGone@users.noreply.github.com>
2024-10-02 12:32:38 -0300

    feat: credits scene

---
+++ Assets/Managers/CreditsManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreditsManager : MonoBehaviour
+{
+        private const string MENU_SCENE = "Menu";
+
+        [SerializeField] private Button _actionReturn;
+
+        private void Start()
+        {
+                _actionReturn.onClick.AddListener(()=> LoadSceneByName(MENU_SCENE));
+        }
+
+        private void LoadSceneByName(string name) => SceneManager.LoadScene(name);
+}
