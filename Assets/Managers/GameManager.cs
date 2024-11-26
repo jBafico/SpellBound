0f5140df822f6e6527af1517ffb33cf0de61d9d2
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-11-26 16:34:21 -0300

    feat: Completed game flow

--- Assets/Managers/GameManager.cs
+++ Assets/Managers/GameManager.cs
@@ -2,16 +2,17 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
         #region GAME_MANAGER_PROPERTIES
 
-        private const string MENU_SCENE = "Menu";
+        private const string DEFEAT_SCENE = "Defeat";
         private const string LOADING_SCENE = "Loading";
+        private const string VICTORY_SCENE = "Victory";
         [SerializeField] private bool _isGameOver = false; //si termino el juego
         [SerializeField] private bool _isVictory = false; //si se obtuvo victoria
-        [SerializeField] private TMP_Text _gameOverMessage;
         [SerializeField] private String _nextLevel;
         
 
@@ -21,24 +22,31 @@ public class GameManager : MonoBehaviour
 
         private void Start()
         {
-                //seteo basico de mensaje, pero vacio
-                _gameOverMessage.text = String.Empty;
                 //Subscripcion al evento
                 EventsManager.Instance.OnGameOver += OnGameOver;
         }
 
         private void Update()
         {
-                if (_isGameOver && !_isVictory && Input.GetKey(KeyCode.Escape))
+                if (_isGameOver && !_isVictory)
                 {
                         Time.timeScale = 1;
-                        SceneManager.LoadScene(MENU_SCENE);
+                        SceneManager.LoadScene(DEFEAT_SCENE);
                 }
-                else if (_isGameOver && _isVictory && Input.GetKey(KeyCode.Space))
+                else if (_isGameOver && _isVictory)
                 {
                         Time.timeScale = 1;
-                        LoadingManager.NEXT_LEVEL = _nextLevel;
-                        SceneManager.LoadScene(LOADING_SCENE);
+
+                        if (_nextLevel == "Victory")
+                        {
+                                SceneManager.LoadScene(VICTORY_SCENE);
+                        }
+                        else
+                        {
+                                LoadingManager.NEXT_LEVEL = _nextLevel;
+                                SceneManager.LoadScene(LOADING_SCENE);
+                        }
+                        
                 }
         }
 
@@ -52,10 +60,6 @@ public class GameManager : MonoBehaviour
                 //Aca tengo que frenar todo
                 Time.timeScale = 0;
                 _isVictory = isVictory;
-                
-                //Preparar el mensaje de texto
-                _gameOverMessage.text = isVictory ? "VICTORY!\n PRESS ESC TO GO TO MENU" : "DEFEAT\n PRESS ESC TO GO TO MENU";
-                _gameOverMessage.color = isVictory ? Color.green : Color.red;
         }
 
         #endregion
