a295147c6e9dec3ba93926bc1062bca6d55bb045
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-12-18 17:24:39 -0300

    feat: Intro finishes on itself

--- Assets/Managers/IntroManager.cs
+++ Assets/Managers/IntroManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -15,8 +16,15 @@ public class IntroManager : MonoBehaviour
         LoadingManager.NEXT_LEVEL = "Tutorial";
                 
         _actionPlay.onClick.AddListener(()=> LoadSceneByName(LEVEL_1_SCENE));
+        StartCoroutine(EndIntro());
     }
 
     private void LoadSceneByName(string name) => SceneManager.LoadScene(name);
+    
+    private IEnumerator EndIntro()
+    {
+        yield return new WaitForSeconds(48f);
+        LoadSceneByName(LEVEL_1_SCENE);
+    }
     
 }
